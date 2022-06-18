f6e744077f17ebabfdfb07acd4b5015284e7d0ec
Jae Park <85603334+jaepark13@users.noreply.github.com>
2022-06-18 00:14:39 -0500

    Create quickSort.py

---
+++ recursionANDsorting/quickSort.py
@@ -0,0 +1,10 @@
+"""Implement quick sort in Python.
+Input a list.
+Output a sorted list."""
+def quicksort(array):
+    arr = array
+    
+    return []
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(quicksort(test))
